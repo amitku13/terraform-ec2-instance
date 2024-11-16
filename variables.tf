513be4487b20f5f351c948d770bc1ef32626eafe
amitku13 <amitkumarbhontele@gmail.com>
2024-11-17 01:24:21 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,14 @@
-variable "ami" {
-  description = "ami-012967cc5a8c9f891"
-}
-
 variable "instance_type" {
+  type        = string
   description = "t2-micro"
 }
 
-variable "instance_name" {
-  description = "amit_ec2"
+variable "ami_id" {
+  type        = string
+  description = "ami-012967cc5a8c9f891"
+}
+
+variable "key_pair_name" {
+  type        = string
+  description = "mykeypair"
 }
