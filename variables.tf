474819b9b66eff4166cb7e374c242ed21e2fdfb0
amitku13 <amitkumarbhontele@gmail.com>
2024-11-17 01:08:23 +0530

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "ami" {
+  description = "ami-012967cc5a8c9f891"
+}
+
+variable "instance_type" {
+  description = "t2-micro"
+}
+
+variable "instance_name" {
+  description = "amit_ec2"
+}
