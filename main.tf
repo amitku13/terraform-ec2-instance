9ea05503e47c0b0c8e1d36f54e8c4716d61643fa
amitku13 <amitkumarbhontele@gmail.com>
2024-11-18 22:19:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,15 @@
 resource "aws_instance" "example" {
-  ami           = var.ami_id
+  ami           = var.ami
   instance_type = var.instance_type
-  key_name      = var.key_pair_name
+  tags = {
+    Name = var.instance_name
+  }
+}
+
+output "instance_id" {
+  value = aws_instance.example.id
+}
+
+output "public_ip" {
+  value = aws_instance.example.public_ip
 }
