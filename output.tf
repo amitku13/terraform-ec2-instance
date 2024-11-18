1f9093e6b7214c16afc9a1159fb7c9f38e0cc959
amitku13 <amitkumarbhontele@gmail.com>
2024-11-18 23:11:18 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,9 +1,9 @@
 output "instance_id" {
-  description = "ID of the EC2 instance"
   value       = aws_instance.example.id
+  description = "The ID of the created EC2 instance."
 }
 
 output "public_ip" {
-  description = "Public IP of the EC2 instance"
   value       = aws_instance.example.public_ip
+  description = "The public IP of the created EC2 instance."
 }
